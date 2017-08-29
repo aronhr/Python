3137e680ca0627de61b5d5713e0d154a9a451088
Aron Hrafnsson <aronhr98@gmail.com>
2017-08-29 23:42:24 +0000

    Fix if number are in list but not in correct place
    Made text clearer.
    Shorten code
    Commented lilbit

--- Aron/Verkefni_1/MasterConsole.py
+++ Aron/Verkefni_1/MasterConsole.py
@@ -1,35 +1,35 @@
-from random import randint
+from random import randint  # Get Random
 
-def numInList(list1, list2):
+# Checks if numbers match the generated numbers from computer.
+def numInList(computerList, userList):
     num = 0
-    for x in xrange(0, len(list1)):
-        for a in xrange(0, len(list1)):
-            if list2[x] == list1[a]:
-                num = num + 1
-
-def numInPlace(list1, list2):
-    num = 0
-    for x in xrange(0, len(list1)):
-        if list1[x] == list2[x]:
+    for a in xrange(0, len(computerList)):
+        if computerList[a] in userList:
             num = num + 1
     return num
 
-computerList = [randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9)]
-userList = []
-win = False
 
-while win == False:
+# Checks if numbers are in correct place.
+def numInPlace(computerList, userList):
+    num = 0
+    for x in xrange(0, len(computerList)):
+        if computerList[x] == userList[x]:
+            num = num + 1
+    return num
 
+# Computer gets 5 random numbers
+computerList = [randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9)]
+userList = []   # List to save user inputs
+
+while True:
     for x in xrange(1, 6):
-        userList.append(raw_input("Select number " + str(x) + " "))
+        userList.append(input("Select number " + str(x) + " "))
 
     if numInPlace(computerList, userList) == 5:
         print "You Win!"
         break
 
-    print str(numInList(computerList, userList))
-    print "Correct number in wrong place"
+    print str(numInList(computerList, userList)) + " Numbers are correct but in wrong place"
 
-    print str(numInPlace(computerList, userList))
-    print "In correct place"
+    print str(numInPlace(computerList, userList)) + " Numbers are in correct place"
     break
