b90dc7c648d6a01b409382226e9bca0212e36f36
Aron Hrafnsson <aronhr98@gmail.com>
2017-09-07 18:34:16 +0000

    Fix game for Python 3

--- Verkefni_1/MasterConsole.py
+++ Verkefni_1/MasterConsole.py
@@ -3,7 +3,7 @@ from random import randint  # Get Random
 # Checks if numbers match the generated numbers from computer.
 def numInList(computerList, userList):
     num = 0
-    for a in xrange(0, len(computerList)):
+    for a in range(0, len(computerList)):
         if computerList[a] in userList:
             num = num + 1
     return num
@@ -12,7 +12,7 @@ def numInList(computerList, userList):
 # Checks if numbers are in correct place.
 def numInPlace(computerList, userList):
     num = 0
-    for x in xrange(0, len(computerList)):
+    for x in range(0, len(computerList)):
         if computerList[x] == userList[x]:
             num = num + 1
     return num
@@ -22,14 +22,14 @@ computerList = [randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9), rand
 userList = []   # List to save user inputs
 
 while True:
-    for x in xrange(1, 6):
+    for x in range(1, 6):
         userList.append(input("Select number " + str(x) + " "))
 
     if numInPlace(computerList, userList) == 5:
-        print "You Win!"
+        print("You Win!")
         break
 
-    print str(numInList(computerList, userList)) + " Numbers are correct but in wrong place"
+    print(str(numInList(computerList, userList)) + " Numbers are correct but in wrong place")
 
-    print str(numInPlace(computerList, userList)) + " Numbers are in correct place"
+    print(str(numInPlace(computerList, userList)) + " Numbers are in correct place")
     break
